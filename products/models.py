8fcf7c3c0ab1a071e9da6d1cfb0b7572ec462506
LaisKor <spiderjiwon@nate.com>
2024-04-30 10:49:17 +0900

    gitignore 수정->products/MEDIA 폴더 업로드

--- products/models.py
+++ products/models.py
@@ -1,9 +1,28 @@
+import os
 from django.db import models
 from django.conf import settings
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/MEDIA')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='products', on_delete=models.CASCADE)
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+        super().delete(*args, **kwargs) 
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            try:
+                old_file = Product.objects.get(pk=self.pk).image
+                if old_file and old_file != self.image:
+                    if os.path.isfile(old_file.path):
+                        os.remove(old_file.path)
+            except Product.DoesNotExist:
+                pass 
+        super().save(*args, **kwargs) 
+
 
